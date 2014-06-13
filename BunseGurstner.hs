476b9435e882d6789a831404ee553ef6714922a0
Ben Gamari <bgamari.foss@gmail.com>
2014-06-13 10:29:18 -0400

    Diagonalization working?

--- BunseGurstner.hs
+++ BunseGurstner.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 -- | Bunse-Gerstner simultaneous diagonalization
 --
 -- Method due to,
@@ -15,11 +18,15 @@ module BunseGurstner
 
 import Prelude hiding (sum, concatMap)
 import Linear
+import Control.Applicative
 import Control.Lens
 import Data.Ord
 import Data.Complex hiding (conjugate)
 import Data.Foldable
+
 import System.IO.Unsafe
+import Debug.Trace
+import Unsafe.Coerce
 
 data Diagonalization f a = Diag { diagA, diagB, diagQ :: !(f (f a)) }
 
@@ -38,8 +45,8 @@ simultDiag a b = iterate go (Diag (toComplex a) (toComplex b) eye3)
   where
     toComplex :: (RealFloat a, Functor f) => f (f a) -> f (f (Complex a))
     toComplex = fmap (fmap realToFrac)
-    
-    go d = foldl' rotPlane d [(ex,ey), (ex,ez), (ey,ez)] 
+
+    go d = foldl' rotPlane d $ [(ex,ey), (ex,ez), (ey,ez)]
 
     rotPlane :: (Conjugate a, RealFloat a, Epsilon a, Show a)
              => Diagonalization V3 (Complex a)    -- ^ initial diagonalization
@@ -50,10 +57,11 @@ simultDiag a b = iterate go (Diag (toComplex a) (toComplex b) eye3)
              (adjoint r !*! b !*! r)
              (q !*! r)
       where
-        --(c, s) = rotationAngle a b (i,j)
-        (c, s) = bruteForceAngle 100 100 a b (i,j)
+        (c, s) = rotationAngle a b (i,j)
+        --(c, s) = bruteForceAngle 1000 1000 a b (i,j)
+        --(c, s) = deepenAngle 100 100 a b (i,j)
         --(c, s) = optimizeAngle a b (i,j)
-        r = unsafePerformIO (print (c,s) >> return (rotation i j c s))
+        r = rotation i j c s
 {-# INLINE simultDiag #-}
 
 -- | Terminate the diagonalization when the off-axis norm has pass
@@ -62,12 +70,14 @@ offAxisTerminate :: (Conjugate a, RealFloat a)
                  => a                                -- ^ epsilon
                  -> [Diagonalization V3 (Complex a)] -- ^ iterates
                  -> [Diagonalization V3 (Complex a)]
-offAxisTerminate eps = takeWhile f
-  where f d = let off = offAxisNorm (diagA d) + offAxisNorm (diagB d)
-                  mag = frobeniusNorm (diagA d) + frobeniusNorm (diagB d)
-              in off > eps * mag
+offAxisTerminate eps (d:rest)
+  | off < eps * mag = d : take 1 rest
+  | otherwise       = d : offAxisTerminate eps rest
+  where
+    off = offAxisNorm (diagA d) + offAxisNorm (diagB d)
+    mag = frobeniusNorm (diagA d) + frobeniusNorm (diagB d)
 {-# INLINE offAxisTerminate #-}
- 
+
 -- | Weight of components off-axis
 offAxisNorm :: (Additive f, Traversable f, Trace f, RealFloat a)
             => f (f (Complex a)) -> a
@@ -75,18 +85,18 @@ offAxisNorm = frobeniusNorm . offAxis
   where
     offAxis a = a !-! kronecker (diagonal a)
 {-# INLINE offAxisNorm #-}
- 
+
 -- | a rotation in ij plane
-rotation :: (Conjugate a, RealFloat a)
-         => E V3 -> E V3 -> a -> Complex a -> M33 (Complex a)
+rotation :: (Conjugate a, RealFloat a, Traversable f, Applicative f)
+         => E f -> E f -> a -> Complex a -> f (f (Complex a))
 rotation _ _ c s | (>1e-5) $ c^2 + (magnitude s)^2 - 1 =
     error "Invalid rotation"
 rotation (E ei) (E ej) c s =
-    eye3
-    + realToFrac (c - 1) *!! (unit ei `outer` unit ei)
-    - conjugate s        *!! (unit ei `outer` unit ej)
-    +           s        *!! (unit ej `outer` unit ei)
-    + realToFrac (c - 1) *!! (unit ej `outer` unit ej)
+      ei . ei %~ (+ realToFrac (c - 1))
+    $ ei . ej .~ (- conjugate s)
+    $ ej . ei .~ s
+    $ ej . ej %~ (+ realToFrac (c - 1))
+    $ kronecker (pure 1)
 {-# INLINE rotation #-}
 
 bruteForceAngle :: (Conjugate a, Ord a, RealFloat a)
@@ -97,15 +107,44 @@ bruteForceAngle :: (Conjugate a, Ord a, RealFloat a)
                 -> (a, Complex a)
 bruteForceAngle nTheta nPhi a b plane =
     minimumBy (comparing $ cond a b plane) $ do
-        theta <- range (-pi/4) (pi/4) nTheta
-        phi <- range (-pi) pi nPhi
+        theta <- linSpace (-pi/4) (pi/4) nTheta
+        phi <- linSpace (-pi) pi nPhi
         return (cos theta, cis phi * realToFrac (sin theta))
-  where
-    range :: RealFrac a => a -> a -> Int -> [a]
-    range a b n = [a + d*realToFrac i | i <- [0..n]]
-      where d = (b - a) / realToFrac n
 {-# INLINE bruteForceAngle #-}
-    
+
+linSpace :: RealFrac a => a -> a -> Int -> [a]
+linSpace a b n = [a + d*realToFrac i | i <- [0..n]]
+  where d = (b - a) / realToFrac n
+{-# INLINE linSpace #-}
+   
+trf :: String -> (a -> String) -> a -> a
+
+trf msg f x = Debug.Trace.trace (msg++f x) x
+asT = unsafeCoerce :: a -> (Double, Double)
+
+deepenAngle :: forall a. (Conjugate a, Ord a, RealFloat a)
+            => Int -> Int
+            -> M33 (Complex a)
+            -> M33 (Complex a)
+            -> (E V3, E V3)
+            -> (a, Complex a)
+deepenAngle nTheta nPhi a b plane = toSinCos $ trf "sc = " (show . asT)
+                                  $ head $ drop 8 $ go 1 (0,0)
+  where
+    toSinCos :: (a, a) -> (a, Complex a)
+    toSinCos (th,ph) = (cos th, cis ph * realToFrac (sin th))
+
+    go :: Int -> (a, a) -> [(a, a)]
+    go n (cTheta,cPhi) =
+      let m :: (a,a)
+          m = minimumBy (comparing $ cond a b plane . toSinCos) $ do
+            theta <- linSpace (cTheta - pi/4/n') (cTheta + pi/4/n') nTheta
+            phi <- linSpace (cPhi - pi/n') (cPhi + pi/n') nPhi
+            return (theta, phi)
+          n' = realToFrac n
+      in m : go (2*n) m
+{-# INLINE deepenAngle #-}
+
 {-
 optimizeAngle :: (Conjugate a, Ord a, RealFloat a)
               => M33 (Complex a)
@@ -123,9 +162,9 @@ gradDescent :: (f a -> a) -> f a -> [f a]
 gradDescent f x = go 1 x (finiteDiff 1 x)
   where
     go h x diffLast =
-    
+
     grad :: V2 a -> State (f a) a
-    grad x = do 
+    grad x = do
         h <- get
         kronecker h
 -}
@@ -172,6 +211,7 @@ rotationAngle a b plane =
     $ concatMap (\f->[f a plane, f b plane])
     -- $ [goldstineAngle, eberleinAngle]
     $ [eberleinAngle]
+{-# INLINE rotationAngle #-}
 
 -- | Minimizing rotation angle due to Eberlein, 1962
 eberleinAngle :: (Conjugate a, Ord a, RealFloat a)
@@ -184,7 +224,8 @@ eberleinAngle a (E i, E j) = (c, realToFrac s)
     x = atan2 (realPart $ a^.i.j + a^.j.i) (realPart $ a^.i.i - a^.j.j) / 2
     c = cos x
     s = sin x
-  
+{-# INLINE eberleinAngle #-}
+
 -- | Minimizing rotation angle due to Goldstine & Horwitz, 1958 (pg. 179)
 goldstineAngle :: (Conjugate a, Ord a, RealFloat a, Epsilon a)
                => M33 (Complex a)
@@ -202,7 +243,7 @@ goldstineAngle a (E i, E j) = (cos phi, cis alpha * realToFrac (sin phi))
     s i j = magnitude (c ^. i . j)
     gamma i j = phase (c ^. i . j)
     v = s i i - s j j
-    -- 
+    --
     t = u :+ v
     kappa = u^2 + v^2 - 4*((r i j)^2 * (cos $ beta i j - alpha)^2 + (s i j)^2 * (sin $ gamma i j - alpha)^2)
     lambd = 4 * (r i j * u * cos (beta i j - alpha) + s i j * v * sin (gamma i j - alpha))
@@ -214,7 +255,8 @@ goldstineAngle a (E i, E j) = (cos phi, cis alpha * realToFrac (sin phi))
                 num = (r i j + s i j)^2 * sin v
                 denom = (r i j + s i j)^2 * (sin v)^2 + (r i j - s i j)^2 * (cos v)^2
             in (asin (-num / sqrt denom) - beta i j - gamma i j) / 2
+{-# INLINE goldstineAngle #-}
 
 frobeniusNorm :: (Foldable f, Functor f, RealFloat a) => f (f (Complex a)) -> a
 frobeniusNorm a = sum $ fmap (sum . fmap (\x->(magnitude x)^2)) a
-{-# INLINE frobeniusNorm #-}              
+{-# INLINE frobeniusNorm #-}
--- DiagTest.hs
+++ DiagTest.hs
@@ -8,27 +8,33 @@ import Data.List (intercalate)
 import BunseGurstner
 
 main = do
-    mapM (\d->do print "hello"
-                 print $ offAxisNorm $ diagA d
-                 putStrLn $ showMatrix $ diagQ d !*! adjoint (diagQ d)
-                 --putStrLn $ showMatrix $ adjoint (diagQ d) !*! fmap (fmap realToFrac) m !*! diagQ d
-                 --putStrLn $ showMatrix $ diagQ d !*! fmap (fmap realToFrac) m !*! adjoint (diagQ d)
-                 putStrLn $ showMatrix $ diagA d
-                 putStrLn $ showMatrix $ diagB d
-         )
-      $ take 20 $ drop 00000
-      $ offAxisTerminate 0.01 $ simultDiag n m
+    mapM (\(i,d)->do
+      print ("hello", i, offAxisNorm $ diagA d, offAxisNorm $ diagB d)
+      putStrLn $ showMatrix $ diagQ d !*! adjoint (diagQ d)
+      --putStrLn $ showMatrix $ adjoint (diagQ d) !*! fmap (fmap realToFrac) m !*! diagQ d
+      --putStrLn $ showMatrix $ diagQ d !*! fmap (fmap realToFrac) m !*! adjoint (diagQ d)
+      putStrLn $ showMatrix $ diagA d
+      putStrLn $ showMatrix $ diagB d
+      mapRotation (show i) (diagA d) (diagB d)
+      )
+      $ zip [0..] $ take 40 $ drop 00000
+      $ offAxisTerminate 1e-7
+      $ simultDiag n m
 
+toComplex :: RealFloat a => M33 a -> M33 (Complex a)
 toComplex = fmap (fmap realToFrac)
 
-mapRotation = do
-    writeRot "xy.txt" (ex,ey)
-    writeRot "xz.txt" (ex,ez)
-    writeRot "yz.txt" (ey,ez)
+mapRotation :: (Conjugate a, RealFloat a, Show a)
+            => String -> M33 (Complex a) -> M33 (Complex a) -> IO ()
+mapRotation t a b = do
+    writeRot ("xy-"++t++".txt") (ex,ey)
+    writeRot ("xz-"++t++".txt") (ex,ez)
+    writeRot ("yz-"++t++".txt") (ey,ez)
+    return ()
   where
     writeRot name plane = 
       writeFile name $ unlines
-      $ map (\(a,b,c)->intercalate "\t" $ map show [a,b,c]) $ values plane
+      $ map (\(a,b,c)->intercalate "\t" $ map show [a,b,c]) $ values a b plane
     nTheta = 100
     nPhi = 100
 
@@ -36,31 +42,25 @@ mapRotation = do
     range a b n = [a + d*realToFrac i | i <- [0..n]]
       where d = (b - a) / realToFrac n
 
-    values :: (E V3, E V3) -> [(Double, Double, Double)]
-    values plane = do
+    --values :: RealFrac a => M33 a -> M33 a -> (E V3, E V3) -> [(a, a, a)]
+    values a b plane = do
       theta <- range (-pi/4) (pi/4) nTheta
       phi <- range (-pi) pi nPhi
       let rot = (cos theta, cis phi * realToFrac (sin theta))
-      let v = cond (toComplex n) (toComplex m) plane rot
+      let v = cond a b plane rot
       return (theta, phi, v)
     
 showMatrix :: (Functor f, Foldable f, Show a) => f (f a) -> String
 showMatrix a = unlines $ toList $ fmap (foldMap (pad 70 . show)) a
   where pad n s = take n $ s ++ repeat ' '
 
-p :: V3 (V3 Double)
-p = V3 (V3 1 1 0)
-       (V3 1 3 2)
-       (V3 0 2 1)
+u = axisAngle (V3 0 1 0) (35*pi/180)
 
-pinv = case inv33 p of Just a -> a
+vs = fromQuaternion u !*! eye3
 
-m,n :: V3 (V3 Double)
-(m,n) = case commuting3 2 1 1 of
-          m:n:_ -> (herm m, herm n)
-   where
-     herm a = (a + adjoint a) !!* scale
-       where scale = recip $ sum $ fmap (sum . (^2)) a
+n,m :: M33 Double
+m = vs !*! kronecker (V3 8 6 1) !*! adjoint vs
+n = vs !*! kronecker (V3 7 2 3) !*! adjoint vs
 
 preconditions a b = 
     [ norm (commutator a b)
@@ -70,17 +70,3 @@ preconditions a b =
   where norm = sum . fmap (sum . fmap (^2))
     
 commutator a b = a !*! b !-! b !*! a
-
--- | Family of commutative matricies
---
--- From,
---
---     E Pereira, C Rosa. "A method to construct sets of commuting
---     matrices"
-commuting3 :: Num a => a -> a -> a -> [M33 a]
-commuting3 x1 x2 x3 = iterate (^+^ eye3) a
-  where
-    a = V3 (V3 (x1+x2-x3)  (4*x3-x2)  (2*x2)   )
-           (V3 (x2-2*x3)   (x1-x2)     x3      )
-           (V3 (-4*x3)     (4*x3)     (x1+2*x3))
-           
